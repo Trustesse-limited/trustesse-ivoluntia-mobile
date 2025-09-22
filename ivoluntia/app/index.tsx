9ca0f6c1c9aca1947bd90915677d65b62108cb0d
Fruity3010 <dblur3010@gmail.com>
2025-09-22 02:50:57 +0100

    Initial project setup with Expo, navigation, and dafault structure

---
+++ ivoluntia/app/index.tsx
@@ -0,0 +1,23 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default function Onboarding() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hey Trustees</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  text: {
+    fontSize: 28,
+    fontWeight: "bold",
+    color: "#333",
+  },
+});
